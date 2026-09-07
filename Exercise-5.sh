4174c45ac9f3b66ee7954085cdc397270025ee04
Imre Bodnár <bodnar.imre.85@gmail.com>
2026-09-07 16:26:50 +0200

    Added Exercise-5

---
+++ Exercise-5.sh
@@ -0,0 +1,20 @@
+# Storing the username in an environment variable
+export TESTNAME="imre"
+
+# Read and store user input
+read -p "Please type 'mem' or 'cpu' to sort the process list! " sortType
+read -p "Please type the number of rows! " rowNumber
+
+# Increment rowNumber because of header row
+((rowNumber++))
+
+# Listing all the processes of the given user
+if [[ $sortType == 'mem' ]]
+then
+    ps -U $TESTNAME -u $TESTNAME u --sort=-%mem | head -n $rowNumber
+elif [[ $sortType == 'cpu' ]]
+then
+    ps -U $TESTNAME -u $TESTNAME u --sort=-%cpu | head -n $rowNumber
+else
+    echo "Unknown input!"
+fi
