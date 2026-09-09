f4f273b7ea058e0c569990e13333733ee2016b42
Imre Bodnár <bodnar.imre.85@gmail.com>
2026-09-09 14:57:25 +0200

    Added Exercise-7

---
+++ Exercise-7.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#netstat -lntp | grep node
+
+./Exercise-6.sh
+
+isRunning=false
+# Checking whether the app is running or stopped
+if pgrep -x "node" > /dev/null
+then
+    echo "The NodeJS app is RUNNING!"
+    isRunning=true
+else
+    echo "The NodeJS app is STOPPED!"
+fi
+
+if [[ isRunning ]]
+then
+    # Showing the running process of the app
+    echo -e "\nThe running app process:"
+    ps aux | grep "node server.js" | head -n 1
+
+    # Showing the listening port of the app
+    echo -e "\nThe running app listening on port: "
+    ss -tnlp | grep node | awk '{print substr($4,3,4)}'
+fi
